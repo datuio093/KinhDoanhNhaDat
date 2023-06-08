99779851c854f44a630c0d9ca0b90499db0cdafe
datuio093 <datthanhphan21@gmail.com>
2023-06-08 11:36:43 +0700

    test

--- chothuenha/settings.py
+++ chothuenha/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'chothuenha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
